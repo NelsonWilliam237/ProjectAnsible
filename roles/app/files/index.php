e5f6446e96963e1f71fd88ac6cac92de151089a1
NelsonWilliam237 <keleko.nelson@gmail.com>
2026-04-15 15:56:38 -0400

    update

--- roles/app/files/index.php
+++ roles/app/files/index.php
@@ -1,9 +1,13 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Connexion base de données
 $host     = getenv('DB_HOST') ?: '10.0.2.13';
 $dbname   = getenv('DB_NAME') ?: 'app_db';
 $username = getenv('DB_USER') ?: 'app_user';
-$password = getenv('DB_PASSWORD') ?: 'AppPass456!';
+$password = getenv('DB_PASSWORD');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
